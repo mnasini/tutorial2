34a2519574e98e7f85e771a94b5034034707e524
Michele Nasini <michele.nasini98@gmail.com>
2021-01-30 01:49:15 +0100

    added the possibility of accepting user input

--- tutorial5/main_ex_3.cpp
+++ tutorial5/main_ex_3.cpp
@@ -2,11 +2,27 @@
 #include "exercise3.hpp"
 using namespace std;
 int main(){
+	float x1;
+	float y1;
+	float x2;
+	float y2;
 	Point p1=Point();
 	Point p2=Point(2,2);
 	p1.display();
 	p2.display();
-	p1.distance(p2);
+	cout<<"now we will test the distance between two user defined objects points: "<<endl<<"insert the x1(x of the first point):"<<endl;
+	cin>>x1;
+	cout<<"insert y1:"<<endl;
+	cin>>y1;
+	cout<<"insert x2:"<<endl;
+	cin>>x2;
+	cout<<"insert y2:"<<endl;
+	cin>>y2;
+	Point p3=Point(x1,y1); //first object point 
+	Point p4=Point(x2,y2); //second object point
+	p3.distance(p4);       //calling the method distance from point (giving as a parameter another point)
+	
+	
 
 	
 	system("pause");
