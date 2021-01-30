5ca6b3a5f6c6ba12421cc7951c3e8a5843c4fde8
Michele Nasini <michele.nasini98@gmail.com>
2021-01-30 01:19:23 +0100

    added distance method

--- tutorial5/exercise1.cpp
+++ tutorial5/exercise1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 
@@ -12,22 +13,32 @@ class Point{
 			y=0;
 			
 		}
-		Point(int a,int b){
+		Point(float a,float b){
 			x=a;
 			y=b;
 		}
 		void display(){
 			cout<<"the x is "<<x<<" and the y is "<<y<<endl;
-		}
+		} 
+		 void distance(Point other){
+			float val=0.0;
+			val=sqrt(pow(other.x-x,2)+pow(other.y-y,2));
+			cout<<"the distance between the tho points is "<<val<<endl;
+			
+	}
 		
 	
 	
 };
+
+
 int main(){
 	Point p1=Point();
-	Point p2=Point(1,2);
+	Point p2=Point(2,2);
 	p1.display();
 	p2.display();
+	p1.distance(p2);
+
 	
 	
 	return 0;
