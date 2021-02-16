37ab548577c55377341a1c92f41f737e57e01b4c
Michele Nasini <michele.nasini98@gmail.com>
2021-02-16 02:35:04 +0100

    second point complete

--- tutorial7/exercise_1.cpp
+++ tutorial7/exercise_1.cpp
@@ -66,11 +66,20 @@ class Patient{
 		}
 		void printReport(){
 			int f=0;
+			int som=0;
+			int max=0;
+			int index;
+			float aver;
 			cout<<"the highest abnormal blood pressures of "<<name<<":"<<endl;
 			for(int i=0;i<data.size();i++){
+				som=som+data[i].showdiastol();
+				if(data[i].showsyst()>=max){
+					max=data[i].showsyst();
+					index=i;
+				}
 				if(data[i].showsyst()>140){
 					f++;
-					cout<<"day: "<<data[i].showdate().getd()<<"/"<<data[i].showdate().getm()<<"/"<<data[i].showdate().gety()<<" systolic value: "<<data[i].showsyst()<<" distolic value: "<<data[i].showdiastol()<<endl;
+					cout<<"day: "<<data[i].showdate().getd()<<"/"<<data[i].showdate().getm()<<"/"<<data[i].showdate().gety()<<" systolic value: "<<data[i].showsyst()<<" diastolic value: "<<data[i].showdiastol()<<endl;
 				}
 				
 				
@@ -78,6 +87,9 @@ class Patient{
 			if (f==0){
 					cout<<"no measurement was too high"<<endl;
 				}
+				aver=float(som)/float(data.size());
+				cout<<"the average diastolic blood pressure was: "<<aver<<endl;
+				cout<<"the highest systolic pressure record is: "<<endl<<"day: "<<data[index].showdate().getd()<<"/"<<data[index].showdate().getm()<<"/"<<data[index].showdate().gety()<<" systolic value: "<<data[index].showsyst()<<" diastolic value: "<<data[index].showdiastol()<<endl;
 			
 		}
 		
